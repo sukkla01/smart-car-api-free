bcd5f63477571c022a5ea8986e6ac8e439ceedd1
sukkla01 <sukkla01@gmail.com>
2023-09-27 16:16:39 +0700

    xx

--- config.js
+++ config.js
@@ -2,14 +2,14 @@
 module.exports = {
     secret: '1234',
     dbOption: {
-        host: '192.168.0.32',
-        user: 'ict009',
-        password: 'Ss0810432245',
+        host: '192.168.0.xxx',
+        user: 'xxxx',
+        password: 'xxxx',
         database: 'smart_car',
         port: '3306',
         charset: 'utf8mb4'
     },
-    line_token : 'YpJg7hMJVMNOXbw2yCmmZjWAFyrs3U5Fau7pxi2J5BG'
+    line_token : 'YpJg7hMJVMNOXbw2yCmmZjWAxFyrs3U5Fau7pxi2J5BGxxxxxx'
 }
 
 
